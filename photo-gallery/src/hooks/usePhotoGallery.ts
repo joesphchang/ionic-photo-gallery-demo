93a9ef1a0f2abee135b23cef8d53487b44896c21
joesphchang <joesphschang@gmail.com>
2025-06-11 10:14:24 -0500

    Completed saving photos page

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -8,10 +8,26 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 
-// Create a function named usePhotoGallery
+/**
+ * 
+ * @returns filepath, webviewPath, photos, takePhoto, 
+ */
 export function usePhotoGallery() {
     const [photos, setPhotos ] = useState<UserPhoto[]>([]);
-    const fileName = Date.now() + '.jpeg';
+    
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        const base64Data = await base64FromPath(photo.webPath!);
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+        });
+
+        return {
+            filepath: fileName,
+            webviewPath: photo.webPath,
+        };
+    }
     
     const takePhoto = async () => {
         const photo = await Camera.getPhoto({
@@ -19,23 +35,40 @@ export function usePhotoGallery() {
             source: CameraSource.Camera,
             quality: 100,
         });
-
-        const newPhotos = [
-            {
-                filepath: fileName,
-                webviewPath: photo.webPath,
-            },
-            ...photos,
-        ];
+        
+        const fileName = Date.now() + '.jpeg';
+        const savedFileImage = await savePicture(photo, fileName);
+        const newPhotos = [savedFileImage, ...photos];
         setPhotos(newPhotos);
     };
-    
-
 
     return {
         photos,
         takePhoto,
     };
+}
+
+/**
+ * The base64FromPath method is a helper util that downloads a file from the supplied path
+ * and returns a base64 representation of that file.
+ * @param path 
+ * @returns 
+ */
+export async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+            if (typeof reader.result === 'string') {
+                resolve(reader.result);
+            } else {
+                reject('method did not return a string');
+            }
+        };
+        reader.readAsDataURL(blob);
+    });
 }
 
 // Create a userPhoto function
